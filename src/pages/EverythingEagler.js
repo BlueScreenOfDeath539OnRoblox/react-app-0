95e93057f7fb8ab9e9a5baeb9a8e737ffc0cf372
IAmMyGuy21th <159368595+IAmMyGuy21th@users.noreply.github.com>
2025-08-12 09:40:47 -0700

    added eagler0

--- src/pages/EverythingEagler.js
+++ src/pages/EverythingEagler.js
@@ -2,23 +2,15 @@ import { useState } from 'react';
 import './App.css';
 
 function Eagler0() {
-    const [opened, setOpened] = useState(false);
-    const [inputList, setInputList] = useState([]);
-
-    const listitems = () => {
-        const inputs = document.querySelector("#inp").value;
-        setInputList(inputs.split(' '));
-    };
-
     return (
         <div className="App">
+            <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
             <widgetbot
                 server="1399876207838756906"
                 channel="1399876209395110152"
                 width="800"
                 height="600"
             ></widgetbot>
-            <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
         </div>
     );
 }
