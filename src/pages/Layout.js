cbd8266bb114b1dcfe3a2cdc5d908d75755ac7f5
IAmMyGuy21th <159368595+IAmMyGuy21th@users.noreply.github.com>
2025-09-05 17:42:23 -0700

    fix glassmorphism page

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,42 +4,45 @@ import "./Layout.css"; // Import the CSS file
 const Layout = () => {
     return (
         <>
-            <nav className="main-navbar">
-                <ul className="main-navbar-list">
-                    <li>
-                        <Link to="/">App</Link>
-                    </li>
-                    <li>
-                        <Link to="/other">Other</Link>
-                    </li>
-                    <li>
-                        <Link to="/testing">Test and Stuff</Link>
-                    </li>
-                    <li>
-                        <Link to="/discordclone">DiscordClone example</Link>
-                    </li>
-                    <li>
-                        <Link to="/plat0">Platformer Test</Link>
-                    </li>
-                    <li>
-                        <Link to="/symbols">3d Testing</Link>
-                    </li>
-                    <li>
-                        <Link to="/eagler0">Everything Eaglercraft</Link>
-                    </li>
-                    <li>
-                        <Link to="/links">Links Collection</Link>
-                    </li>
-                    <li>
-                        <Link to="/add-link">Add Link</Link>
-                    </li>
-                    <li>
-                        <Link to="glassmorphism">Glassmorphism</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="layout-container">
+                <div className="hover-box"></div>
+                <nav className="main-navbar">
+                    <ul className="main-navbar-list">
+                        <li>
+                            <Link to="/">App</Link>
+                        </li>
+                        <li>
+                            <Link to="/other">Other</Link>
+                        </li>
+                        <li>
+                            <Link to="/testing">Test and Stuff</Link>
+                        </li>
+                        <li>
+                            <Link to="/discordclone">DiscordClone example</Link>
+                        </li>
+                        <li>
+                            <Link to="/plat0">Platformer Test</Link>
+                        </li>
+                        <li>
+                            <Link to="/symbols">3d Testing</Link>
+                        </li>
+                        <li>
+                            <Link to="/eagler0">Everything Eaglercraft</Link>
+                        </li>
+                        <li>
+                            <Link to="/links">Links Collection</Link>
+                        </li>
+                        <li>
+                            <Link to="/add-link">Add Link</Link>
+                        </li>
+                        <li>
+                            <Link to="glassmorphism">Glassmorphism</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-            <Outlet />
+                <Outlet />
+            </div>
         </>
     )
 };
